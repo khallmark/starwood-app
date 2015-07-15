0f8cc21ebf4f44573cc615a32a88ece0ce6f7d71
Kevin Hallmark <kevin.hallmark@littleblackhat.com>
2015-07-15 15:50:25 -0400

    Making a contacts app instead.

---
+++ contacts/www/js/app.js
@@ -0,0 +1,68 @@
+/*global angular */
+
+/**
+ * The main TodoMVC app module
+ *
+ * @type {angular.Module}
+ */
+var app = {
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+		app.receivedEvent('deviceready');
+
+
+		angular.module('contacts', ['ngRoute'])
+			.config(function ($routeProvider) {
+				'use strict';
+
+				var routeConfig = {
+					controller: 'ContactCtrl',
+					templateUrl: 'todomvc-index.html',
+					resolve: {
+						store: function (todoStorage) {
+							// Get the correct module (API or localStorage).
+							return todoStorage.then(function (module) {
+								module.get(); // Fetch the todo records in the background.
+								return module;
+							});
+						}
+					}
+				};
+
+				$routeProvider
+					.when('/', routeConfig)
+					.when('/:status', routeConfig)
+					.otherwise({
+						redirectTo: '/'
+					});
+			});
+
+	},
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
+
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');
+
+		console.log('Received Event: ' + id);
+	}
+};
+
+app.initialize();
---
+++ contacts/www/js/services/contactStorage.js
@@ -0,0 +1,68 @@
+
+angular.module('contacts')
+	.factory('contactStorage', function ($http, $injector) {
+		'use strict';
+
+		return $injector.get('localStorage');
+	})
+	.factory('localStorage', function ($q) {
+		'use strict';
+
+		var STORAGE_ID = 'contacts-angularjs';
+
+		var store = {
+			contacts: [],
+
+			_getFromLocalStorage: function () {
+				return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+			},
+
+			_saveToLocalStorage: function (contacts) {
+				localStorage.setItem(STORAGE_ID, JSON.stringify(contacts));
+			},
+
+			delete: function (contact) {
+				var deferred = $q.defer();
+
+				store.contacts.splice(store.contacts.indexOf(contact), 1);
+
+				store._saveToLocalStorage(store.contacts);
+				deferred.resolve(store.contacts);
+
+				return deferred.promise;
+			},
+
+			get: function () {
+				var deferred = $q.defer();
+
+				angular.copy(store._getFromLocalStorage(), store.contacts);
+				deferred.resolve(store.contacts);
+
+				return deferred.promise;
+			},
+
+			insert: function (contact) {
+				var deferred = $q.defer();
+
+				store.contacts.push(contact);
+
+				store._saveToLocalStorage(store.contacts);
+				deferred.resolve(store.contacts);
+
+				return deferred.promise;
+			},
+
+			put: function (contact, index) {
+				var deferred = $q.defer();
+
+				store.contacts[index] = contact;
+
+				store._saveToLocalStorage(store.contacts);
+				deferred.resolve(store.contacts);
+
+				return deferred.promise;
+			}
+		};
+
+		return store;
+	});
